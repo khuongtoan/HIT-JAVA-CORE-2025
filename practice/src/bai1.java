03f1784dad1cd165eb7693274a5d157d5ec11f7a
khuongtoan <kxtdumbo@gmail.com>
2025-07-27 22:06:16 +0700

    testing push on github

---
+++ practice/src/bai1.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+
+public class bai1 {
+    public static void main(String[] args) {
+        String a = "         abc   ";
+        System.out.println(a.trim()+" kkkkkk");
+    }
+}
